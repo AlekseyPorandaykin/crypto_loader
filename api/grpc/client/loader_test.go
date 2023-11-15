8e6e29fc4a04bbf2428c3b9aeeeba2d7199cb9b3
alexey.porandaikin <porandusy@mail.ru>
2023-11-15 10:57:48 +0200

    Add clients

---
+++ api/grpc/client/loader_test.go
@@ -0,0 +1,28 @@
+package client
+
+import (
+	"context"
+	"fmt"
+	"testing"
+)
+
+func TestLoader_Start(t *testing.T) {
+	ctx, cancel := context.WithCancel(context.Background())
+	l := DefaultLoader()
+
+	go func() {
+		defer cancel()
+		if err := l.Start(ctx, 10); err != nil {
+			t.Error(err)
+			return
+		}
+	}()
+	for {
+		select {
+		case <-ctx.Done():
+			return
+		case batch := <-l.Batch():
+			fmt.Printf("Prices: %d \n", len(batch.Prices()))
+		}
+	}
+}
