4d46de001d159478076ffb6e7e27f9fe61bfad6b
alexey.porandaikin <alexey.porandaikin@exness.com>
2025-06-17 08:18:53 +0300

    Add rate limit control

--- pkg/bybit/v5/sender/basic.go
+++ pkg/bybit/v5/sender/basic.go
@@ -3,6 +3,8 @@ package sender
 import (
 	"go.uber.org/zap"
 	"net/http"
+	"strconv"
+	"sync"
 	"time"
 )
 
@@ -13,10 +15,14 @@ type HTTPDoer interface {
 type Basic struct {
 	httpClient HTTPDoer
 	logger     *zap.Logger
+
+	mu              sync.Mutex
+	counterRequests int
+	lastWindow      time.Time
 }
 
 func NewBasic() *Basic {
-	return &Basic{httpClient: http.DefaultClient}
+	return &Basic{httpClient: http.DefaultClient, lastWindow: time.Now().Truncate(time.Minute)}
 }
 
 func (s *Basic) WithHttpClient(httpClient HTTPDoer) {
@@ -38,5 +44,19 @@ func (s *Basic) Send(req *http.Request) (Response, error) {
 	resp, err := s.httpClient.Do(req)
 	result := NewResponse(resp)
 	result.AddAction("Duration http transport", time.Since(start).String())
+	result.AddAction("Current request counter", strconv.Itoa(s.increaseCounter()))
 	return result, err
+}
+
+func (s *Basic) increaseCounter() int {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+	now := time.Now().Truncate(time.Minute)
+	if s.lastWindow.Before(now) {
+		s.lastWindow = now
+		s.counterRequests = 1
+	} else {
+		s.counterRequests++
+	}
+	return s.counterRequests
 }
